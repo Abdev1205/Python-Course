50e4529c57e5f0ca0e4d334e96ec0c6bcb73e908
Abhay1205Mishra <abhaym1205@gmail.com>
2024-02-07 12:09:41 +0530

    added string sliced code

---
+++ class-work/string/Activity-1-6-JAN.py
@@ -0,0 +1,15 @@
+string1 = "hello"
+string2 = "India"
+ans1 = string1 + " " + string2
+print(ans1, "\n")
+
+
+# print("writing 3 times \n ", (ans1 * 3))
+# printing 3 times by using for loop
+for i in range(3):
+    print(ans1, "\n")
+
+# slicing the string of ans1
+slicedAns1 = ans1[2:]
+# there are many diffrent steps to get differente type of sliced if string
+print(slicedAns1, "\n")
